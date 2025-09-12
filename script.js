caaac78d67476ce05d1769452ad146c5423eb8d3
bigrudy <ayobelajarbks01@gmail.com>
2025-09-13 01:16:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function salam() {
+    alert("Terima kasih sudah berkunjung ke Usaha Kuliner BigRudy!");
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    // Tampilkan pesan setelah 2 detik
+    setTimeout(salam, 2000);
+  });
+  
